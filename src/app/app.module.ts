209d3aaae7a62f26f23e63ee53e80dca0f2e3d8a
AyeshaSumrin <ayesha-sumrin.showkath-pasha@partners.basf.com>
2021-05-12 23:21:16 +0530

    Created contact-list component and updated css files to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactListComponent } from './contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
